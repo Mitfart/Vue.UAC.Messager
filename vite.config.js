18076ff057a44659a63c518452959c40189e6c61
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-04-20 21:06:54 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -10,4 +9,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: '/Vue.UAC.Messager/'
 })
