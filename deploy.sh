ffdf6c1cc2fb2fb942985706d463eb7c34122aa3
Mitfart <mr.mitfart@mail.ru>
2023-04-20 09:28:23 +0300

    Fix: Desploy -> Username

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mitfart/Vue.UAC.Messager.git master:gh-pages
+git push -f git@github.com:Mitfart/Vue.UAC.Messager.git master:gh-pages
 
 cd -
