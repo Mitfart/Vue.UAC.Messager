cdf19c0890645899f5ccaffae9b4999157e40ebb
Mitfart <mr.mitfart@mail.ru>
2023-04-20 09:24:09 +0300

    GitPages Setup

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+      ? 'Vue.UAC.Messager/'
+      : '/'
 })
