634f5962cc662b609319f3b3000bfe3593a74497
Mitfart <Mitfart@users.noreply.github.com>
2023-04-20 06:45:14 +0000

    deploy: 27bf23328d8cd845c053801a42c7693465e8ec34

---
+++ assets/PublishPostPage-8208dfd4.js
@@ -0,0 +1,53 @@
+import{P as v,I as p}from"./InputField-4e5bd3ba.js";import{_ as r,o as t,c,e as b,w as i,h as k,d as f,a as e,i as x,b as o,F as g,r as C,t as M,p as y,j as P,A as I}from"./index-3c6a0915.js";const B=["cols","rows","placeholder"],S={__name:"TextareaField",props:{cols:{type:Number,required:!1},rows:{type:Number,required:!1},placeholder:{type:String,required:!1}},setup(n){return(_,a)=>(t(),c("textarea",{cols:n.cols,rows:n.rows,placeholder:n.placeholder},null,8,B))}},A=r(S,[["__scopeId","data-v-c0487afe"]]);const V={__name:"SecondaryBtn",setup(n){return(_,a)=>(t(),b(v,{class:"btn_secondary"},{default:i(()=>[k(_.$slots,"default",{},()=>[f(" Secondary ")],!0)]),_:3}))}},h=r(V,[["__scopeId","data-v-8a8e0f54"]]),F={},z={class:"icon",xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor"},N=e("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:`M15.182 15.182a4.5 4.5 0 01-6.364
+          0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.
+          75 9.75c0 .414-.168.75-.375.75S9 10.164
+          9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375
+          0h.008v.015h-.008V9.75zm5.625 0c0
+          .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375
+          0h.008v.015h-.008V9.75z`},null,-1),j=[N];function H(n,_){return t(),c("svg",z,j)}const T=r(F,[["render",H]]),D={},q={class:"icon",xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor"},L=e("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:`M3.375 19.5h17.25m-17.25 0a1.125 1.125 0
+          01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6
+          18.996 6 18.375m-3.75 0V5.625m0
+          12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375
+          2.625V5.625m0 12.75c0 .621-.504 1.125-1.125
+          1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0
+          3.75h-1.5A1.125 1.125 0 0118 18.375M20.625
+          4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625
+          4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0
+          .621-.504 1.125-1.125 1.125M3.375 4.5c-.621
+          0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004
+          6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0
+          0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0
+          .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6
+          7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6
+          9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125
+          4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5
+          0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125
+          1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504
+          1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125
+          0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496
+          5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18
+          10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504
+          1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125
+          1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18
+          18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18
+          13.125v1.5c0 .621.504 1.125 1.125 1.125M18
+          13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0
+          .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496
+          12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125
+          1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0
+          0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125
+          1.125m-17.25 0h1.5m14.25 0h1.5`},null,-1),R=[L];function O(n,_){return t(),c("svg",q,R)}const E=r(D,[["render",O]]),G={},J={class:"icon",xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor"},K=e("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:`M2.25 15.75l5.159-5.159a2.25 2.25 0
+          013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25
+          2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5
+          0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5
+          0 002.25 6v12a1.5 1.5 0 001.5
+          1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375
+          0 11-.75 0 .375.375 0 01.75 0z`},null,-1),Q=[K];function U(n,_){return t(),c("svg",J,Q)}const W=r(G,[["render",U]]),X={},Y={class:"icon",xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor"},Z=e("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:`M15.75 17.25v3.375c0 .621-.504 1.125-1.125
+          1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125
+          1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5
+          10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06
+          9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125
+          1.125v3.5m7.5 10.375H9.375a1.125 1.125 0
+          01-1.125-1.125v-9.25m12 6.625v-1.875a3.375
+          3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0
+          01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75`},null,-1),e1=[Z];function o1(n,_){return t(),c("svg",Y,e1)}const n1=r(X,[["render",o1]]),s1={},t1={class:"icon",xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor"},c1=e("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M6 18L18 6M6 6l12 12"},null,-1),_1=[c1];function l1(n,_){return t(),c("svg",t1,_1)}const r1=r(s1,[["render",l1]]);const m=n=>(y("data-v-8772ce0e"),n=n(),P(),n),a1={class:"publish-form block"},i1=m(()=>e("h1",{class:"publish-form__title"}," Опубликовать пост ",-1)),d1={class:"publish-form__label"},h1=m(()=>e("h2",null," Заголовок ",-1)),u1={class:"publish-form__label"},m1=m(()=>e("h2",null," Содержание ",-1)),p1={class:"publish-form__media"},v1={class:"publish-form__buttons"},f1={class:"loaded-media__container"},w1={class:"loaded-media rect"},$1={class:"loaded-media__content"},b1={class:"loaded-media__overflow"},k1={class:"publish-form__label"},x1=m(()=>e("h2",null," Теги ",-1)),g1={__name:"PublishPostForm",setup(n){const a=x([]);function u(l){a.indexOf(l)<0?(a.push(l),console.log("Add "+l)):console.log("CANT Add "+l)}function w(l){const s=a.indexOf(l);s>=0?(a.splice(s,1),console.log("Remove "+l)):console.log("CANT Remove "+l)}function $(){console.log("Dummy log")}return(l,s)=>(t(),c("div",a1,[i1,e("label",d1,[h1,o(p,{class:"publish-form__field",placeholder:"Введите заголовок"})]),e("label",u1,[m1,o(A,{class:"publish-form__field",placeholder:"Введите Содержание",rows:15})]),e("div",p1,[e("div",v1,[o(h,{onClick:s[0]||(s[0]=d=>u("_1"))},{default:i(()=>[o(T)]),_:1}),o(h,{onClick:s[1]||(s[1]=d=>u("_2"))},{default:i(()=>[o(E)]),_:1}),o(h,{onClick:s[2]||(s[2]=d=>u("_3"))},{default:i(()=>[o(W)]),_:1}),o(h,{onClick:s[3]||(s[3]=d=>u("_4"))},{default:i(()=>[o(n1)]),_:1})]),e("div",f1,[(t(!0),c(g,null,C(a,d=>(t(),c("div",w1,[e("div",$1,M(d),1),e("div",b1,[o(h,{class:"loaded-media__btn rect btn_active",onClick:P1=>w(d)},{default:i(()=>[o(r1)]),_:2},1032,["onClick"])])]))),256))])]),e("label",k1,[x1,o(p,{class:"publish-form__field",placeholder:"Введите теги (до 3 шт.)"})]),e("footer",null,[o(v,{onClick:$},{default:i(()=>[f(" Отправить ")]),_:1})])]))}},C1=r(g1,[["__scopeId","data-v-8772ce0e"]]);const M1={class:"publish-post"},y1={__name:"PublishPostPage",setup(n){return(_,a)=>(t(),c("div",M1,[o(I,{class:"container_content"},{default:i(()=>[o(C1,{class:"publish-post__form"})]),_:1})]))}},S1=r(y1,[["__scopeId","data-v-0b403f4f"]]);export{S1 as default};
