27bf23328d8cd845c053801a42c7693465e8ec34
Mitfart <mr.mitfart@mail.ru>
2023-04-20 09:44:29 +0300

    Fix: Vue.Desply -to> Vite.Desploy

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
+  base: process.env.NODE_ENV === 'production'
       ? '/Vue.UAC.Messager/'
       : '/'
 })
